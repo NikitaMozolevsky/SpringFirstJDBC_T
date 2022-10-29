57aff60de7320b776766d036b48fdbc70859bcde
Nikita Mozolevsky <mozolevsky.nikita@mail.ru>
2022-10-29 11:18:56 +0300

    Second commit

--- src/main/java/com/nikita/lessons/config/SpringConfig.java
+++ src/main/java/com/nikita/lessons/config/SpringConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,12 +14,19 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import javax.sql.DataSource;
+
 @Configuration
 @ComponentScan("com.nikita.lessons")
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer { /** replacement for applicationContext.xml */
 
     private final ApplicationContext applicationContext;
+
+    public static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    public static final String DRIVER_PATH = "org.postgresql.Driver";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "1111";
 
     @Autowired
     public SpringConfig(ApplicationContext applicationContext) {
@@ -47,5 +55,22 @@ public class SpringConfig implements WebMvcConfigurer { /** replacement for appl
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(DRIVER_PATH);
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- src/main/java/com/nikita/lessons/dao/impl/PersonDaoImpl.java
+++ src/main/java/com/nikita/lessons/dao/impl/PersonDaoImpl.java
@@ -2,60 +2,32 @@ package com.nikita.lessons.dao.impl;
 
 import com.nikita.lessons.dao.PersonDao;
 import com.nikita.lessons.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDaoImpl implements PersonDao {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "1111";
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public static final String SELECT_PEOPLE_SQL = "SELECT * FROM Person";
     public static final String INSERT_INTO_PERSON_SQL = "INSERT INTO Person VALUES (?, ?, ?, ?)";
     public static final String SELECT_PERSON_BY_ID_SQL = "SELECT * FROM Person WHERE Person.id = ?";
     public static final String UPDATE_PERSON_SQL = "UPDATE Person SET name = ?, age = ?, email = ? WHERE id = ?";
     public static final String DELETE_PERSON_SQL = "DELETE FROM Person WHERE id = ?";
 
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
     public List<Person> getPeople() {
-        List<Person> people = new ArrayList<>();
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(SELECT_PEOPLE_SQL)) {
 
-            while (resultSet.next()) {
-                Person person = new Person();
-
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-
-                people.add(person);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return people;
+        return jdbcTemplate.query(SELECT_PEOPLE_SQL, );
     }
 
     public Person getPerson(int id) {
